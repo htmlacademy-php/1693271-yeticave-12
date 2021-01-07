8c498c7c124804e2c6c98bf520c99523170f245a
Ильгиз <76611492+Ilgiz-ssg@users.noreply.github.com>
2021-01-07 20:47:06 +0500

    update menu

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = ''; // укажите здесь ваше имя
+$user_name = 'Ильгиз'; 
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -27,10 +27,26 @@ $user_name = ''; // укажите здесь ваше имя
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-
-        <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
-
-        </nav>
+            <?php if($is_auth == 1) {
+            echo '
+                    <div class="user-menu__logged">
+                        <p>#Ilgiz#</p>
+                        <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
+                        <a class="user-menu__logout" href="#">Выход</a>
+                    </div>
+                    ';
+                     } elseif($is_auth == 0) {
+                         echo '
+                    <ul class="user-menu__list">
+                        <li class="user-menu__item">
+                             <a href="#">Регистрация</a>
+                        </li>
+                        <li class="user-menu__item">
+                            <a href="#">Вход</a>
+                         </li>
+                  </ul>';
+                }?>
+            </nav>
     </div>
 </header>
 
