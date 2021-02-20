5c1604cb787645735736ac180dfa746a1cc99294
Ильгиз <76611492+Ilgiz-ssg@users.noreply.github.com>
2021-02-20 20:08:24 +0500

    function

--- index.php
+++ index.php
@@ -134,7 +134,8 @@ $user_name = 'Ильгиз';
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<?php
+                            <span class="lot__cost">цена<
+                            <?php
                                                             var_dump(round($int, 3));
                                                             number_format ( float $<="1000" , int $decimals = 3 , string|null $decimal_separator = "." , string|null $thousands_separator = "," ) : string
                                                             number_format ( float $>="1000" , int $decimals = 0 , string|null $decimal_separator = "." , string|null $thousands_separator = "," ) : string
